0d61ab1acba8f1d1ed5af39f84269b5e5af8e5f6
Andrew Player <andrewplayer3@gmail.com>
2024-09-04 15:50:02 -0500

    get bboxs and dem

--- src/hyp3_srg/timeseries.py
+++ src/hyp3_srg/timeseries.py
@@ -7,6 +7,10 @@ import logging
 from pathlib import Path
 from typing import Iterable, Optional
 
+from shapely import unary_union
+
+from hyp3_srg import dem, utils
+
 log = logging.getLogger(__name__)
 
 
@@ -24,6 +28,17 @@ def timeseries(
         bucket_prefix: Add a bucket prefix to the product(s)
         work_dir: Working directory for processing
     """
+    if work_dir is None:
+        work_dir = Path.cwd()
+
+    bboxs = []
+    for granule in granules:
+        bboxs.append(utils.get_bbox(granule))
+
+    full_bbox = unary_union(bboxs).buffer(0.1)
+    dem_path = dem.download_dem_for_srg(full_bbox, work_dir)
+    utils.create_param_file(dem_path, dem_path.with_suffix('.dem.rsc'), work_dir)
+
     pass
 
 
@@ -32,8 +47,8 @@ def main():
 
     Example command:
     python -m hyp3_srg ++process timeseries \
-        S1A_IW_RAW__0SDV_20231229T134339_20231229T134411_051870_064437_4F42.geo \
-        S1A_IW_RAW__0SDV_20231229T134404_20231229T134436_051870_064437_5F38.geo
+        S1A_IW_RAW__0SDV_20231229T134339_20231229T134411_051870_064437_4F42 \
+        S1A_IW_RAW__0SDV_20231229T134404_20231229T134436_051870_064437_5F38
     """
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--bucket', help='AWS S3 bucket HyP3 for upload the final product(s)')
