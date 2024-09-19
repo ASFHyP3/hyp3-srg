c61e6f260218319551476da80ae384c6f95c8e06
Andrew Player <andrewplayer3@gmail.com>
2024-09-19 15:52:57 -0500

    updated tests

--- tests/test_dem.py
+++ tests/test_dem.py
@@ -13,33 +13,18 @@ def test_download_dem_for_srg(monkeypatch):
         m.setattr(dem, 'ensure_egm_model_available', mock_ensure_egm_model_available)
         mock_call_stanford_module = mock.Mock()
         m.setattr(utils, 'call_stanford_module', mock_call_stanford_module)
-        dem.download_dem_for_srg(box(0, 1, 2, 3), Path.cwd())
+        dem.download_dem_for_srg(box(0, 1, 2, 3).bounds, Path.cwd())
         mock_ensure_egm_model_available.assert_called_once()
         mock_call_stanford_module.assert_called_once_with(
             'DEM/createDEMcop.py',
             [str(Path.cwd() / 'elevation.dem'), str(Path.cwd() / 'elevation.dem.rsc'), 3, 1, 0, 2],
             work_dir=Path.cwd(),
         )
-
-
-def test_download_dem_from_bounds(monkeypatch):
-    with monkeypatch.context() as m:
-        mock_ensure_egm_model_available = mock.Mock()
-        m.setattr(dem, 'ensure_egm_model_available', mock_ensure_egm_model_available)
-        mock_call_stanford_module = mock.Mock()
-        m.setattr(utils, 'call_stanford_module', mock_call_stanford_module)
-        dem.download_dem_from_bounds([1.0, 0.0, -1.0, 1.0], Path.cwd())
-        mock_ensure_egm_model_available.assert_called_once()
-        mock_call_stanford_module.assert_called_once_with(
-            'DEM/createDEMcop.py',
-            [str(Path.cwd() / 'elevation.dem'), str(Path.cwd() / 'elevation.dem.rsc'), 1.0, 0.0, -1.0, 1.0],
-            work_dir=Path.cwd(),
-        )
         bad_bboxs = [
             [0.0, 1.0, -1.0, 1.0],
-            [1.0, 1.0, -1.0, 1.0],
-            [1.0, 0.0, 1.0, -1.0]
+            [1.0, 1.0, -1.0, 2.0],
+            [1.0, 0.0, 2.0, -1.0]
         ]
         for bbox in bad_bboxs:
             with pytest.raises(ValueError, match=r'Improper bounding box formatting*'):
-                dem.download_dem_from_bounds(bbox, Path.cwd())
+                dem.download_dem_for_srg(bbox, Path.cwd())
---
+++ tests/test_time_series.py
@@ -0,0 +1,45 @@
+from unittest import mock
+
+import pytest
+
+from hyp3_srg import time_series, utils
+
+
+def test_create_time_series_product_name():
+    granule_names = [
+        'S1A_IW_RAW__0SDV_001_003_054532_06A2F8_8276.zip',
+        'S1A_IW_RAW__0SDV_004_005_054882_06AF26_2CE5.zip',
+        'S1A_IW_RAW__0SDV_010_020_055057_06B527_1346.zip'
+    ]
+    bounds = [-100, 45, -90, 50]
+    name = time_series.create_time_series_product_name(granule_names, bounds)
+    assert name.startswith('S1_SRG_SBAS_35_W100_0_N45_0_W090_0_N50_0_001_010')
+
+    bounds = [101.5123, -34.333, 56.866, -25.8897]
+    name = time_series.create_time_series_product_name(granule_names, bounds)
+    assert name.startswith('S1_SRG_SBAS_35_E101_5_S34_3_E056_9_S25_9_001_010')
+
+
+def test_get_size_from_dem(tmp_path):
+    rsc_content = """
+    WIDTH          1235
+    FILE_LENGTH    873
+    X_FIRST        -124.41472222
+    Y_FIRST        39.52388889
+    X_STEP         0.0027777778
+    Y_STEP         -0.0027777778
+    X_UNIT        degrees
+    Y_UNIT        degrees
+    Z_OFFSET      0
+    Z_SCALE       1
+    PROJECTION    LL
+    xstart         1
+    ystart         1
+    xsize          12357
+    ysize          8731"""
+
+    rsc_path = tmp_path / 'elevation.dem.rsc'
+    with open(rsc_path, 'w') as rsc_file:
+        rsc_file.write(rsc_content.strip())
+    dem_width, dem_height = time_series.get_size_from_dem(dem_file=rsc_path)
+    assert dem_width, dem_height == (1235, 873)
