4a8d440a0c8cdb1506ccf39bae9c15c0b90acd22
Jake Herrmann <jtherrmann@alaska.edu>
2024-10-14 11:27:00 -0800

    add todo

--- src/hyp3_srg/back_projection.py
+++ src/hyp3_srg/back_projection.py
@@ -109,6 +109,7 @@ def back_project(
         work_dir: Working directory for processing
         gpu: Use the GPU-based version of the workflow
     """
+    # TODO: remove this
     assert granules[0] != 'S1A_IW_RAW__0SDV_20240723T020812_20240723T020844_054882_06AF26_2CE5'
 
     if use_gslc_prefix:
