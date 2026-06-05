e8f672c662de3b66c4969e29559051257ee5ccc7
Mario Angarita <mf.angarita52@gmail.com>
2026-06-05 13:26:02 -0800

    fix typo

--- scripts/submit_time_series_job.py
+++ scripts/submit_time_series_job.py
@@ -111,7 +111,7 @@ def get_args():
     parser.add_argument('--bucket', help='Bucket Name')
     parser.add_argument(
         '--bucket-prefix',
-        help="Bucket prefix job products will be written to. The argument will be ignored if `--bucket` isn't provided. This argument also allows expansion of job name or job id by using `{name}` and `{job_id}` if contained within the string.",
+        help="Bucket prefix job products will be written to. The argument will be ignored if `--bucket` isn't provided. This argument also allows expansion of job name or job id by using `{name}` and `{job_id}` within the string.",
     )
     parser.add_argument('--process', choices=['sbas', 'ps'], default='sbas', help='Processing strategy (sbas or ps)')
     parser.add_argument('--tbaseline', default=1000, help='Temporal baseline')
